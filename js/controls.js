35ac86c6ae12cca1866fd8dfd3a45b2f505ee04f
Mayush Tim <mayushtim01@gmail.com>
2025-04-26 23:35:03 -0400

    removed changes

--- js/controls.js
+++ js/controls.js
@@ -4,25 +4,25 @@ const occupationSelect = {
     const occupations = Array.from(
       new Set(surveyData.map((d) => d.Occupation).filter(Boolean))
     ).sort();
-    const dropdown = d3.select('#occupationSelect');
+    const dropdown = d3.select("#occupationSelect");
 
     dropdown
-      .selectAll('option')
+      .selectAll("option")
       .data(occupations)
       .enter()
-      .append('option')
-      .attr('value', (d) => d)
+      .append("option")
+      .attr("value", (d) => d)
       .text((d) => d);
 
     return dropdown;
   },
 
   getSelectedOccupation: function () {
-    return d3.select('#occupationSelect').node().value;
+    return d3.select("#occupationSelect").node().value;
   },
 
   onOccupationChange: function (callback) {
-    d3.select('#occupationSelect').on('change', function () {
+    d3.select("#occupationSelect").on("change", function () {
       callback(this.value);
     });
   },
